f70a54146187d54bdefded009d6056f00e34e670
Ambira <ambirapradhan@gmail.com>
2025-02-23 20:27:45 +0545

    1st assignment

--- configure-networking.sh
+++ configure-networking.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+!/bin/bash
 echo "Configuring Network..."
-read -p "Enter new ip address: " NEW_IPADDRESS
-echo "$NEW_IPADDRESS"
-read -p "Enter Default Gateway..." DEFAULT_GATEWAY
-echo "$DEFAULT_GATEWAY"
+
+read -p "Enter new IP address:" NEW_IPADDRESS
+echo "$NEW_IPADDRESS" 
+read -p "Enter default  gateway:" DEFAULT_GATEWAY
+echo "$DEFAULT_GATEWAY" 
 sudo ip addr add $NEW_IPADDRESS dev enp0s3
 sudo ip route add default via $DEFAULT_GATEWAY
-
