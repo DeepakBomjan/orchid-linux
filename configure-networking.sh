3e64fadd6fe9935ce446c2acd9a071637308f53a
AbhipsaSiwakoti <siwakotiabhipsa@gmail.com>
2025-02-23 10:53:42 +0545

    lab report 1

---
+++ configure-networking.sh
@@ -0,0 +1,11 @@
+!/bin/bash
+echo "Configuring Network..."
+
+read -p "Enter new IP address:" NEW_IPADDRESS
+echo "$NEW_IPADDRESS" 
+read -p "Enter default  gateway:" DEFAULT_GATEWAY
+echo "$DEFAULT_GATEWAY" 
+sudo ip addr add $NEW_IPADDRESS dev enp0s3
+sudo ip route add default via $DEFAULT_GATEWAY
+echo "network updated successfully"  
+
